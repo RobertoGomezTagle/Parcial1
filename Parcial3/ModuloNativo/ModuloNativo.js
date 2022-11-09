44d57e866b6f148a12594928067ea56d7b3de135
RobertoGomezTagle <robertogomez2333@outlook.com>
2022-11-09 15:12:16 -0600

    ModuloNativo

---
+++ Parcial3/ModuloNativo/ModuloNativo.js
@@ -0,0 +1,6 @@
+import calculoArea from './ModuloNativo2.js'
+import {perimetro, PI} from './ModuloNativo2.js'
+
+console.log('area: ' + calculoArea(10));
+console.log('perimetro:' + perimetro(10));
+console.log('PI: ' + PI);
